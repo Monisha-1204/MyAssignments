5b0783ea113f1d3c25fefca20a04aa0dc3745a83
Monisha-1204 <132172184+Monisha-1204@users.noreply.github.com>
2023-05-04 18:52:35 +0100

    Add files via upload
    week1

---
+++ week1/MissingElementInAnArray.java
@@ -0,0 +1,23 @@
+package daily.assignment;
+
+import java.util.Arrays;
+
+public class MissingElementInAnArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,2,3,4,7,6,8};
+		Arrays.sort(arr);
+		int j=0;
+		for(int i=0;i<arr.length;i++) {
+			if(arr.length!=arr[i])
+			{
+				j=i+1;
+				if(arr[i]!=j) {
+					System.out.println("The missing element in an Array is: " +j);
+					break;
+				}	
+			}
+
+		}}}
+
